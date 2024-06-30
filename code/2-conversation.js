766a275a6985781ce030aa962faf0d1723063bc2
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-07-01 10:03:56 +1200

    lots of errors. I'll fix it later

--- code/2-conversation.js
+++ code/2-conversation.js
@@ -3,20 +3,55 @@
 // ####################################
 
 function conversation() {
-  alert('Welcome to my conversation program.')
+  alert('Welcome to my sports discussion program')
 
-  alert('Do you like cycling? Answer yes or no.')
-  let answer = prompt()
+  let answer = prompt('Do you like running? Answer yes or no.')
 
   if (answer == 'yes') {
-    alert("That's good - you will get very fit.")
-  } else {
-    alert('Perhaps you like some other sport.')
+    let ans2 = prompt ("That's good - do you do athletics?")
+
+    if (ans2 == 'yes') {
+      alert ('Good luck in your next race!')
+    } else {
+      alert ('Just for fun then.')
+    }
+  } 
+
+  else {
+    let olympic = prompt ('Do you watch Olympic Athletics?')
+
+    if (olympic == 'yes') {
+      alert ("That's good - It's inspiring.")
+    } else {
+      alert ("It's not for everyone.")
+    }
   }
 
   alert('Goodbye')
 }
 
 function age() {
-  // write your code here
+  let age = prompt ('How old age you?')
+  age = Number (age)
+
+  if (age>65) {
+    alert ("You are old enough to retire. That's sad :(")
+  } else (age<5) {
+    alert ('You still need supervision when using a computer')
+  } else (age >= 18) {
+    alert ('You are old enough to vote. Yippee')
+  } else { (age !=14) {
+    alert ('You are not the same age as me.')
+  }
+  }
+
+
+
+
+
+
+
+
+
+
 }

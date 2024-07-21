b4910e777856fad07d302f641c2e1198e2c86cb9
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2024-07-22 10:04:32 +1200

    sale

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -3,5 +3,24 @@
 // ####################################
 
 function saleCalculator() {
-  // write your code here
+  
+let budget = prompt ('How much money do you have to spend?')
+budget = Number (budget)
+let ogPrice = prompt ('How much is the item that you wish to purchase normally?')
+ogPrice = Number (ogPrice)
+let disPercent = prompt ('And what is the discount percentage?')
+disPercent = Number (disPercent)
+
+price = ogPrice / 100 * disPercent
+
+ans = ogPrice - price
+
+if (ans <= budget) {
+alert ('The Item will cost $' + ans + ' which will mean you will have enough money to purchase the item with the discount of ' + disPercent + '%')
+} else if (ans > budget) {
+ alert ('You do not have enough money. You have $' + budget + ' to spend, but even with ' + disPercent + '% off, $' + ogPrice + ' only comes down to $' + ans )
+} else {
+  alert ('Error')
 }
+}
+
